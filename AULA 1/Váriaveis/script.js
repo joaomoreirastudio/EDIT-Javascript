fe8bbc511f9f5860bae2f72dd4cee1b31f35b78d
joaomoreirastudio <joaomoreirastudio@gmail.com>
2024-05-08 21:25:57 +0100

    Folder Created

---
+++ AULA 1/Váriaveis/script.js
@@ -0,0 +1,22 @@
+const name = "joao";
+let age = 29;
+
+var email = "joao@edit.com";
+const promo = 1.1;
+
+const valorTotal = 100 / promo;
+
+const person = {
+    name: "joão",
+    age: 29,
+    email: "joao@edit.com",
+    Mobile: 91999,
+};
+
+let fullName = "João" + " " + "Moreira";
+console.log(name);
+console.log(age);
+console.log(valorTotal);
+console.log(email);
+console.log(person);
+console.log(typeof fullName);
