8185af78015ccae97ca743f48586e42028a91fb5
João Moreira <joaomoreirastudio@gmail.com>
2024-07-04 23:05:38 +0100

    Feat: folder created and code it's starting to work

---
+++ EXERCICOS-AVALIACAO/Exercicio-final-avaliação/Ecommerce-homepage/script.js
@@ -0,0 +1,46 @@
+const response = fetch("https://fakestoreapi.com/products")
+    .then((Response) => Response.json())
+    .then((data) => {
+        console.log(data);
+        const productList = data;
+        const body = document.body;
+        const productGrid = document.createElement("div");
+        productGrid.id = "product__grid";
+        body.appendChild(productGrid);
+
+        data.forEach((product) => {
+            const productCard = document.createElement("div");
+            productCard.className = "product__card";
+            productCard.innerHTML = `<h2>${product.title}</h2>
+            <div class="image__container"><img src="${product.image}" alt="${product.title}"></div>
+            
+            <button class="btn btn--addToCart">Add to cart</button>`;
+            productGrid.appendChild(productCard);
+
+            // const addToCartButton =
+            //     productCard.querySelector(".btn--addToCart");
+            // addToCartButton.addEventListener("click", () => {
+            //     const userId = 7;
+            //     const date = new Date().toISOString().split("T")[0];
+            //     const productId = product.id;
+            //     const quantity = 1;
+
+            //     const cartData = {
+            //         userId,
+            //         date,
+            //         products: [{ productId, quantity }],
+            //     };
+
+            //     fetch(`https://fakestoreapi.com/carts/7`, {
+            //         method: "PUT",
+            //         headers: { "Content-Type": "application/json" },
+            //         body: JSON.stringify(cartData),
+            //     })
+            //         .then((response) => response.json())
+            //         .then((data) => console.log(data))
+            //         .catch((error) => console.error("error", error));
+            // });
+        });
+    })
+
+    .catch((error) => console.error("error", error));
